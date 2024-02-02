ff8baba4baf79deb5ca2ce682f973a0261b2b25b
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2024-02-02 17:55:24 +0600

    Update 1+1.asm

--- 1+1.asm
+++ 1+1.asm
@@ -15,3 +15,7 @@ _start:
     mov eax, 1          ; syscall number for sys_exit
     xor ebx, ebx        ; exit code 0
     int 0x80            ; call kernel
+
+
+
+;This code first loads the value 1 into the eax register, then adds 1 to it using the add instruction. Finally, it exits the program with an exit code of 0.
